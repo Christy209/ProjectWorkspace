bc64a3b4d36e14a8b6b74107ec3fc98484c2d68b
Christy209 <christinajenifer787@gmail.com>
2025-10-21 12:48:34 +0530

    new new

---
+++ HAMI/src/main/java/Annotation/ExcelData.java
@@ -0,0 +1,17 @@
+package Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExcelData {
+    String sheetName();
+    int[] rowIndex();  // Always as array
+  //  int rowIndex1() default -1;
+    
+	
+	
+}
+
